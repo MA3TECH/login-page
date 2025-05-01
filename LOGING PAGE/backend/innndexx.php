19b563f7c651ab6d01b166ef1d95126a412caf75
elbakar7 <komboabubakar62@gmail.com>
2025-05-01 19:01:55 +0300

    Update innndexx.php

---
+++ LOGING PAGE/backend/innndexx.php
@@ -0,0 +1,14 @@
+7:01:29 PM  [main] 	most application stuff but whenever you do something with services
+7:01:29 PM  [main] 	there will be a security dialogue or things will break! So think 
+7:01:29 PM  [main] 	about running this application with administrator rights!
+7:01:29 PM  [main] 	XAMPP Installation Directory: "c:\xampp\"
+7:01:29 PM  [main] 	Checking for prerequisites
+7:01:34 PM  [main] 	All prerequisites found
+7:01:34 PM  [main] 	Initializing Modules
+7:01:34 PM  [mysql] 	Problem detected!
+7:01:34 PM  [mysql] 	Port 3306 in use by "Unable to open process"!
+7:01:34 PM  [mysql] 	MySQL WILL NOT start without the configured ports free!
+7:01:34 PM  [mysql] 	You need to uninstall/disable/reconfigure the blocking application
+7:01:34 PM  [mysql] 	or reconfigure MySQL and the Control Panel to listen on a different port
+7:01:34 PM  [main] 	Starting Check-Timer
+7:01:34 PM  [main] 	Control Panel Ready
